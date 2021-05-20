000f57935dd7ca536afc00972163cdbc099a27bb
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-19 19:07:27 -0700

    Added result componenet that is used to hold the generated meme picture

---
+++ src/components/Result.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+interface ResultInterface {
+  resultContainerRef: React.RefObject<any>;
+}
+
+const Result = (props: ResultInterface) => {
+  return (
+    <div ref = {props.resultContainerRef} className = "result"></div>
+
+  )
+}
+export default Result
